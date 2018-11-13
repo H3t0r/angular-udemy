c5e28a9f003cf391080f328513624b05c17c0315
H3t0r <isc.quch@gmail.com>
2018-11-13 09:46:55 -0600

    Add spotify service with a getNewReleases endpoint

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private spotifyService: SpotifyService) {
+    this.spotifyService.getNewReleases();
   }
 
+  ngOnInit() {}
 }
---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SpotifyService {
+  constructor(private http: HttpClient) {}
+
+  getNewReleases() {
+    const url = 'https://api.spotify.com/v1/browse/new-releases';
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer BQDRYZqDTSefGk3K4BrBwtJZD-W0bn3prwsuDEG-vZB7Xvcfu0lC0mBvsUhnbSW1PnfW865dFHukan3WHS4',
+    });
+
+    this.http.get(url, { headers }).subscribe(data => console.log(data));
+  }
+}
