27ec2df344f94b373cff042b79202f065ef845fd
H3t0r <isc.quch@gmail.com>
2018-11-22 19:32:51 -0600

    Add AuthGuard service to restrict access to the Protected route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProtectedComponent } from './components/protected/protected.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'protected', component: ProtectedComponent },
+  { path: 'protected', component: ProtectedComponent, canActivate: [AuthGuardService] },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuardService implements CanActivate {
+  constructor(private auth: AuthService) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    console.log(next);
+    if (this.auth.isAuthenticated()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
