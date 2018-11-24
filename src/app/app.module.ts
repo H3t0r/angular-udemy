4054ea546b1362d90012e325e7a981facfdbc2af
H3t0r <isc.quch@gmail.com>
2018-11-24 14:19:12 -0600

    Add logic to handle validations in the template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TemplateFormsComponent } from './components/template-forms/template-forms.component';
 import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TemplateFormsComponent,
-    ReactiveFormsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TemplateFormsComponent, ReactiveFormsComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/template-forms/template-forms.component.ts
+++ src/app/components/template-forms/template-forms.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-forms',
   templateUrl: './template-forms.component.html',
-  styleUrls: ['./template-forms.component.css']
+  styleUrls: ['./template-forms.component.css'],
 })
 export class TemplateFormsComponent implements OnInit {
+  user: Object = {
+    first: null,
+    last: null,
+    email: null,
+    month: '',
+    sex: '',
+  };
 
-  constructor() { }
+  months = [
+    { number: 1, string: 'January' },
+    { number: 2, string: 'February' },
+    { number: 3, string: 'March' },
+    { number: 4, string: 'April' },
+    { number: 5, string: 'May' },
+    { number: 6, string: 'June' },
+    { number: 7, string: 'July' },
+    { number: 8, string: 'August' },
+    { number: 9, string: 'September' },
+    { number: 10, string: 'October' },
+    { number: 11, string: 'November' },
+    { number: 12, string: 'December' },
+  ];
+
+  constructor() {}
 
   ngOnInit() {
+    $('.ui.radio.checkbox').checkbox();
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form);
+    console.log(this.user);
+  }
 }
