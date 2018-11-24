607580e826a576ebec8f59ddee10e773276c5582
H3t0r <isc.quch@gmail.com>
2018-11-23 20:45:21 -0600

    Add semantic ui and reactiveForms and templateForms components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemplateFormsComponent } from './components/template-forms/template-forms.component';
+import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormsComponent,
+    ReactiveFormsComponent
   ],
   imports: [
     BrowserModule
