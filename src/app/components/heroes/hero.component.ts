d319e321bd1801b41059db161c8acef804f33e61
H3t0r <isc.quch@gmail.com>
2018-11-27 23:47:34 -0600

    Update hero template, add the logic to submit the template
    Add hero interface

--- src/app/components/heroes/hero.component.ts
+++ src/app/components/heroes/hero.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Hero } from './hero.interface';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styles: []
+  styles: [],
 })
 export class HeroComponent implements OnInit {
+  hero: Hero = {
+    name: '',
+    bio: '',
+    house: '',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit() {
+    console.log(this.hero);
   }
-
 }
