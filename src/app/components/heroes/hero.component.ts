797688e9878fcdf6614054e3ab4d449cef7b9a94
H3t0r <isc.quch@gmail.com>
2018-11-29 00:12:18 -0600

    Add logic to create new hero, update hero and get a hero

--- src/app/components/heroes/hero.component.ts
+++ src/app/components/heroes/hero.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { Hero } from './hero.interface';
+import { HeroesService } from './heroes.service';
 
 @Component({
   selector: 'app-hero',
@@ -11,14 +13,42 @@ export class HeroComponent implements OnInit {
   hero: Hero = {
     name: '',
     bio: '',
-    house: '',
+    house: 'marvel',
   };
+  id: string;
 
-  constructor() {}
+  constructor(
+    private heroesAPI: HeroesService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.route.params.subscribe(params => {
+      this.id = params.id;
+
+      if (this.id !== 'new') {
+        this.heroesAPI.getHero(this.id).subscribe(hero => (this.hero = hero));
+      }
+    });
+  }
 
   ngOnInit() {}
 
   onSubmit() {
-    console.log(this.hero);
+    if (this.id === 'new') {
+      this.heroesAPI
+        .newHero(this.hero)
+        .subscribe(res => this.router.navigate(['/hero', res.name]), error => console.error(error));
+    } else {
+      this.heroesAPI
+        .updateHero(this.hero, this.id)
+        .subscribe(res => console.log(res), error => console.error(error));
+    }
+  }
+
+  onNew(form: NgForm) {
+    this.router.navigate(['/hero', 'new']);
+    form.reset({
+      house: 'marvel',
+    });
   }
 }
---
+++ src/app/components/heroes/heroes.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Hero } from './hero.interface';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroesService {
+  URL = 'https://herocrud.firebaseio.com/heroes';
+
+  constructor(private http: Http) {}
+
+  newHero(hero: Hero) {
+    const endpoint = `${this.URL}.json`;
+    const body = JSON.stringify(hero);
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+
+    return this.http
+      .post(endpoint, body, { headers })
+      .pipe(map((res: any) => JSON.parse(res._body)));
+  }
+
+  updateHero(hero: Hero, id: string) {
+    const endpoint = `${this.URL}/${id}.json`;
+    const body = JSON.stringify(hero);
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+
+    return this.http.put(endpoint, body, { headers }).pipe(map((res: any) => res));
+  }
+
+  getHero(key$: string) {
+    const endpoint = `${this.URL}/${key$}.json`;
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+
+    return this.http.get(endpoint).pipe(map((res: any) => JSON.parse(res._body)));
+  }
+}
